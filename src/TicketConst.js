4eb85adc612c58bb61b52ab622790888d100aee6
Mew-www <mew.proxy@hotmail.com>
2019-01-29 09:34:15 +0200

    Finalize end-to-end ticket processing

--- src/TicketConst.js
+++ src/TicketConst.js
@@ -28,13 +28,13 @@ const HSL_LAHISEUTU_3 = HSL_HELSINKI.concat(["0102", "0104", "0106", "0109"])
                 .concat(HSL_TUUSULA_KERAVA_SIPOO);
 
 export default [
-  new Ticket("Kertalippu Helsinki", HSL_HELSINKI, 80, 2.20, "HSL", (phone_num, valid_from) => {return {phoneNumber: phone_num, ticketTypeId: "single", customerTypeId: "adult", regionId: "helsinki", validFrom: valid_from}}),
-  new Ticket("Kertalippu Espoo-Kauniainen", HSL_ESPOO_KAUNIAINEN, 80, 2.20, "HSL", (phone_num, valid_from) => {return {phoneNumber: phone_num, ticketTypeId: "single", customerTypeId: "adult", regionId: "espooKauniainen", validFrom: valid_from}}),
-  new Ticket("Kertalippu Vantaa", HSL_VANTAA, 80, 2.20, "HSL", (phone_num, valid_from) => {return {phoneNumber: phone_num, ticketTypeId: "single", customerTypeId: "adult", regionId: "vantaa", validFrom: valid_from}}),
-  new Ticket("Kertalippu Kirkkonummi-Siuntio", HSL_KIRKKONUMMI_SIUNTIO, 80, 2.20, "HSL", (phone_num, valid_from) => {return {phoneNumber: phone_num, ticketTypeId: "single", customerTypeId: "adult", regionId: "kirkkonummiSiuntio", validFrom: valid_from}}),
-  new Ticket("Kertalippu Tuusula-Kerava-Sipoo", HSL_TUUSULA_KERAVA_SIPOO, 80, 2.20, "HSL", (phone_num, valid_from) => {return {phoneNumber: phone_num, ticketTypeId: "single", customerTypeId: "adult", regionId: "keravaSipooTuusula", validFrom: valid_from}}),
-  new Ticket("Kertalippu Lähiseutu 1 (PKS)", HSL_SEUTU, 80, 4.20, "HSL", (phone_num, valid_from) => {return {phoneNumber: phone_num, ticketTypeId: "single", customerTypeId: "adult", regionId: "regional", validFrom: valid_from}}),
-  new Ticket("Kertalippu Lähiseutu 2 (Ei Helsingin kautta)", HSL_LAHISEUTU_2, 80, 4.20, "HSL", (phone_num, valid_from) => {return {phoneNumber: phone_num, ticketTypeId: "single", customerTypeId: "adult", regionId: "bizoneRegion", validFrom: valid_from}}),
-  new Ticket("Kertalippu Lähiseutu 3 (Koko HSL alue)", HSL_LAHISEUTU_3, 80, 6.30, "HSL", (phone_num, valid_from) => {return {phoneNumber: phone_num, ticketTypeId: "single", customerTypeId: "adult", regionId: "trizoneRegion", validFrom: valid_from}})
+  new Ticket("Kertalippu Helsinki", HSL_HELSINKI, 80, 2.20, "HSL", (phone_num, valid_from) => {return {phoneNumber: phone_num, ticketTypeId: "single", customerTypeId: "adult", regionId: "helsinki"}}),//, validFrom: valid_from}}),
+  new Ticket("Kertalippu Espoo-Kauniainen", HSL_ESPOO_KAUNIAINEN, 80, 2.20, "HSL", (phone_num, valid_from) => {return {phoneNumber: phone_num, ticketTypeId: "single", customerTypeId: "adult", regionId: "espooKauniainen"}}),//, validFrom: valid_from}}),
+  new Ticket("Kertalippu Vantaa", HSL_VANTAA, 80, 2.20, "HSL", (phone_num, valid_from) => {return {phoneNumber: phone_num, ticketTypeId: "single", customerTypeId: "adult", regionId: "vantaa"}}),//, validFrom: valid_from}}),
+  new Ticket("Kertalippu Kirkkonummi-Siuntio", HSL_KIRKKONUMMI_SIUNTIO, 80, 2.20, "HSL", (phone_num, valid_from) => {return {phoneNumber: phone_num, ticketTypeId: "single", customerTypeId: "adult", regionId: "kirkkonummiSiuntio"}}),//, validFrom: valid_from}}),
+  new Ticket("Kertalippu Tuusula-Kerava-Sipoo", HSL_TUUSULA_KERAVA_SIPOO, 80, 2.20, "HSL", (phone_num, valid_from) => {return {phoneNumber: phone_num, ticketTypeId: "single", customerTypeId: "adult", regionId: "keravaSipooTuusula"}}),//, validFrom: valid_from}}),
+  new Ticket("Kertalippu Lähiseutu 1 (PKS)", HSL_SEUTU, 80, 4.20, "HSL", (phone_num, valid_from) => {return {phoneNumber: phone_num, ticketTypeId: "single", customerTypeId: "adult", regionId: "regional"}}),//, validFrom: valid_from}}),
+  new Ticket("Kertalippu Lähiseutu 2 (Ei Helsingin kautta)", HSL_LAHISEUTU_2, 80, 4.20, "HSL", (phone_num, valid_from) => {return {phoneNumber: phone_num, ticketTypeId: "single", customerTypeId: "adult", regionId: "bizoneRegion"}}),//, validFrom: valid_from}}),
+  new Ticket("Kertalippu Lähiseutu 3 (Koko HSL alue)", HSL_LAHISEUTU_3, 80, 6.30, "HSL", (phone_num, valid_from) => {return {phoneNumber: phone_num, ticketTypeId: "single", customerTypeId: "adult", regionId: "trizoneRegion"}}),//, validFrom: valid_from}})
   // Missing "day" and "season" tickets -- day tickets have "validity time" of 1..7 and season ones have userId; Check documentation for more info.
 ];
--- src/subcomponents/Tickets.js
+++ src/subcomponents/Tickets.js
@@ -2,19 +2,64 @@ import React from 'react'
 import { push } from 'connected-react-router'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
+import axios from 'axios';
+import jwt_decode from 'jwt-decode'
 
-const Tickets = props => (
-  <div>
-    <p>
-      <button onClick={() => props.gotoSearch()}>Go to Travelling</button>
-    </p>
-    <h1>Tickets</h1>
-    <p>todo</p>
-  </div>
-);
+class Tickets extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loaded: false,
+      ticket_jwts: []
+    };
+  }
+  componentDidMount() {
+    axios.all(
+      this.props.user.tickets.map(ticket => {
+        return axios.post(
+          'http://localhost:3030/getHSL',
+          {ticket_id: ticket.uid, phone_num: ticket.secret},
+          {withCredentials: true}
+        )
+      })
+    )
+      .then(axios.spread((...responses) => {
+        let jwts = [];
+        for (let i=0; i<responses.length; i++) {
+          jwts.push(responses[i].data.ticketData);
+          this.setState({ticket_jwts: jwts, loaded: true});
+        }
+      }))
+      .catch(errors => {
+        console.log(errors);
+      });
+  }
+  render() {
+    return (
+      <div>
+        <p>
+          <button onClick={() => this.props.gotoSearch()}>Go to Travelling</button>
+        </p>
+        <h1>Tickets</h1>
+        {!this.state.loaded ?
+          "Loading . . ."
+          :
+          this.state.ticket_jwts.map(jwt => (
+            <div style={{display: "inline-block"}} key={jwt} dangerouslySetInnerHTML={{__html: jwt_decode(jwt).ticket}} />
+          ))
+        }
+      </div>
+    );
+  }
+};
+// "validFrom":"2019-01-28T14:52:37Z","validTo":"2019-01-28T16:13:37Z","ticketTypeId":"single","customerTypeId":"adult","regionId":"regional","iat":1548690901}
+
+const mapStateToProps = ({ auth }) => ({
+  user: auth.user
+});
 
 const mapDispatchToProps = dispatch => bindActionCreators({
   gotoSearch: () => push('/')
 }, dispatch);
 
-export default connect(null, mapDispatchToProps)(Tickets)
+export default connect(mapStateToProps, mapDispatchToProps)(Tickets)
